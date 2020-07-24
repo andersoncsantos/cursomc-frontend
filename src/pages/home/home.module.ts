70099b3cf074c030cfadd894025d9fce05244e10
Anderson Santos <anderson.santos@iteris.com.br>
2020-07-24 17:01:39 -0300

    Lazy loading

---
+++ src/pages/home/home.module.ts
@@ -0,0 +1,9 @@
+﻿import { IonicPageModule } from 'ionic-angular/module';
+import { NgModule } from '@angular/core';
+import { HomePage } from './home';
+
+@NgModule({
+    declarations: [HomePage],
+    imports: [IonicPageModule.forChild(HomePage)]
+})
+export class HomeModule{}
