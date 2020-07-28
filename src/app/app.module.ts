be3130df96897d9be394c097e6ab07a6592c2190
Anderson Santos <anderson.santos@iteris.com.br>
2020-07-28 14:26:20 -0300

    Tratando erro 403

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoriaService } from '../services/domain/categoria.service';
-import { ErrorInterceptorProvider } from '../interceptor/erro-interceptor';
+import { ErrorInterceptorProvider } from '../interceptor/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/client.service';
